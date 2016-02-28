f63b06c19ea2ff115a33eb067d0cd97308dd488a
Christopher 'Chief' Najewicz <chief@beefdisciple.com>
2016-02-28 17:54:03 -0500

    Use ENV vars for AWS provider

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,6 +1,1 @@
-# Configure the AWS Provider
-provider "aws" {
-    access_key = "${var.aws_access_key}"
-    secret_key = "${var.aws_secret_key}"
-    region = "us-east-1"
-}
+provider "aws" {}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,6 +1,3 @@
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
-
 variable "env" {
 	description = "Environment name (production,staging,development)"
 	default = "production"
