5ee2e69380e93a72f4a4a7671141fb56ca471302
Christopher 'Chief' Najewicz <chief@beefdisciple.com>
2016-02-28 21:15:15 -0500

    Tabs

--- terraform/ecs-task.tf
+++ terraform/ecs-task.tf
@@ -1,4 +1,4 @@
 resource "aws_ecs_task_definition" "movie_api" {
-  family = "${concat("movie-api-",var.env)}"
-  container_definitions = "${file("movie-api-task.json")}"
+	family = "${concat("movie-api-",var.env)}"
+	container_definitions = "${file("movie-api-task.json")}"
 }
